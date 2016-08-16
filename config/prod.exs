22f9a5e7dc26fc638385bde08a163fdf9a4f679b
supernullset <sean.j.williamson@gmail.com>
2016-08-16 13:35:34 -0700

    configure server start on app

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :phoenix_distillery, PhoenixDistillery.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: "localhost", port: {:system, "PORT"}],
   cache_static_manifest: "priv/static/manifest.json",
   server: true,
   root: ".",
